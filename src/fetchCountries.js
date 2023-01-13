075b989b10d799c4c26729867eb95592109f67f4
Сергей Каращук <kspost01@gmail.com>
2023-01-13 03:30:16 +0200

    draft version

---
+++ src/fetchCountries.js
@@ -0,0 +1,16 @@
+// export function fetchCountries(query ='Ukraine') {
+//     const BASE_URL = 'https://restcountries.com/v2/all'
+
+//     return fetch(`${BASE_URL}?fields=${query},capital,population,flags,languages`)
+//     .then(resp => {
+//         console.log(resp)
+//         if (!resp.ok) {
+//             throw new Error(resp.statusText)
+//         }
+//     return resp.json()
+//     }
+//     );
+// };
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 import './css/styles.css';
+// import fetchCountries from './fetchCountries';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+
+function fetchCountries(query ='Ukraine') {
+    const BASE_URL = 'https://restcountries.com/v2/all'
+
+    return fetch(`${BASE_URL}?fields=name=${query},capital,population,flags,languages`)
+    .then(resp => {
+        console.log(resp)
+        if (!resp.ok) {
+            throw new Error(resp.statusText)
+        }
+    return resp.json()
+    }
+    );
+};
+
+const input= document.querySelector('#search-box');
+input.addEventListener('input', countrySearch);
+
+
+
+function countrySearch(evt) {
+    const query = evt.currentTarget.value.trim();
+
+    console.log(query);
+
+    fetchCountries(query).then(data => console.log(data))
+}
+
